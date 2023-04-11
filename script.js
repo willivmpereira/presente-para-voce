3f6f9613f3dd5c24e0a6a6e47d11ab25f06239dc
brouwilliamog <william.pereira@focomultimidia.com>
2023-04-10 21:11:14 -0300

    afs

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const start = document.querySelector("#start");
+const screenTwo = document.querySelectorAll(".two ul li");
+const screenThree = document.querySelectorAll(".three ul li");
+const screenFour = document.querySelectorAll(".four ul li");
 
 start.addEventListener("click", () => {
   document.querySelector(".two").style.display = "flex";
+  document.querySelector(".two").scrollIntoView({ behavior: "smooth" });
+
 });
 
-const screenTwo = document.querySelectorAll(".two ul li");
+
+
 
 screenTwo.forEach((item, index) => {
   item.addEventListener("click", () => {
@@ -26,9 +32,9 @@ screenTwo.forEach(function (div) {
 
 document.querySelector("#button02").addEventListener("click", () => {
   document.querySelector(".three").style.display = "flex";
+  document.querySelector(".three").scrollIntoView({ behavior: "smooth" });
 });
 
-const screenThree = document.querySelectorAll(".three ul li");
 
 screenThree.forEach(function (div) {
   div.addEventListener("click", function () {
@@ -44,9 +50,10 @@ screenThree.forEach(function (div) {
 
 document.querySelector("#button03").addEventListener("click", () => {
   document.querySelector(".four").style.display = "flex";
+  document.querySelector(".four").scrollIntoView({ behavior: "smooth" });
+  
 });
 
-const screenFour = document.querySelectorAll(".four ul li");
 
 screenFour.forEach(function (div) {
   div.addEventListener("click", function () {
@@ -63,6 +70,8 @@ screenFour.forEach(function (div) {
 
 document.querySelector("#button04").addEventListener("click", () => {
   document.querySelector(".five").style.display = "flex";
+  document.querySelector(".five").scrollIntoView({ behavior: "smooth" });
+
 
   const comida = localStorage.getItem("comida");
   const dia = localStorage.getItem("dia");
